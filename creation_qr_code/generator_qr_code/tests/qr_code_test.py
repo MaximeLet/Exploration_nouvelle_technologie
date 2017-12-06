538fd3465b733958b97d539f5b5d833b565d6077
Maxime <maximeletourneau.1@gmail.com>
2017-12-05 19:58:17 -0500

    Modifier mon test

--- creation_qr_code/generator_qr_code/tests/qr_code_test.py
+++ creation_qr_code/generator_qr_code/tests/qr_code_test.py
@@ -10,7 +10,7 @@ from generator_qr_code.creator_qr_file import CreatorQRCode
 class TestQRCodeCreation(TestCase):
 
     def test_create_qr_code_add_save_jpeg(self):
-        creator = CreatorQRCode("This is a test")
+        creator = CreatorQRCode("This is a test", "")
         creator.add_data()
         path = "../qr_code/qr_code.jpg"
         creator.create_qr_image_and_save(path)
